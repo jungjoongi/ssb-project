7d129890ffdb8c9ba9e424444fff75cf382a176b
zx3620 <zx3620@gmail.com>
2018-08-23 09:36:00 +0900

    SSB-Project

--- ssbit/src/main/webapp/html2/board/list.js
+++
@@ -1,9 +0,0 @@
-
-var trTemplateSrc = $("#tr-template").html();
-
-var templateFn = Handlebars.compile(trTemplateSrc);
-
-
-$.getJSON(serverRoot+"/json/board/list", (data) => {
-	$(tableBody).html(templateFn({list:data}))
-}); 
